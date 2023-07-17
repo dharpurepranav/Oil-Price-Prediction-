87f42e549a883579040384b23c68f00138f98507
Pranav Dharpure <dharpurepranav@gmail.com>
2023-07-17 14:12:03 +0530

    Add files via upload

---
+++ oil_prices.app.py
@@ -0,0 +1,46 @@
+#Importing required dependencies
+import streamlit as st
+from datetime import date
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+from plotly import graph_objs as go
+import xlrd 
+
+ 
+
+def main():
+    # Set the app title
+    st.title("Prophet Model Deployment G-3")
+
+    # Apply custom CSS styling
+    st.markdown(
+        """
+        <style>
+            /* Change the font family and color of the heading */
+            .title-wrapper {
+                font-family: 'Arial', sans-serif;
+                color: blue;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+
+df = xlrd.open_workbook('Crude_oil_WTI.xls')
+ 
+
+st.subheader('Raw data')
+st.write(df.head())
+
+
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=df['Date'], y=df['Price']))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+
